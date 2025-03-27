af72ce35bdffdc632717144d8a6f4e9661077c1a
lenahmbae <lenahmbae22@gmail.com>
2025-03-27 17:37:02 +0300

    completes arrow functions

---
+++ index.js
@@ -0,0 +1,8 @@
+// Function expression for divide
+const divide = () => 2000 / 100;
+
+// Arrow function for square
+const square = x => x * x;
+
+// Arrow function for add
+const add = (a, b) => a + b;
